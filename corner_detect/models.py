448702b74bef304074400105050d979e2bfcc45d
bombom cho <drjreric@gmail.com>
2023-11-24 10:25:49 +0900

    conn ai server

--- corner_detect/models.py
+++ corner_detect/models.py
@@ -16,46 +16,43 @@ class PriceTable(models.Model):
 class CornerImage(models.Model):
     picture = models.ImageField()
     picture_id = models.CharField(max_length=14, blank=True)
-    product_name = models.CharField(max_length=200, blank=True, default='')
     info = models.CharField(max_length=200, blank=True)
+    product_name = models.CharField(max_length=200, blank=True, default='')
     uploaded = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "Corner Image took at {}".format(self.uploaded.strftime('%Y-%m-%d %H:%M'))
     
     def save(self, *args, **kargs):
-        # picture_id 설정
-        # new_id = str(round(time.time()))
-        # new_id += str(int(round(random.random(), 3) * 1000))
-        # self.picture_id = new_id
-
         ##### ai 서버로 사진 전송 #####
-        # image_data = self.picture.read()
+        image_data = self.picture.read()
 
-        # target_server_url = "http://ec2-43-201-111-213.ap-northeast-2.compute.amazonaws.com:8000/api-corner-ai/corner_ai/"
+        target_server_url = "http://ec2-43-201-249-72.ap-northeast-2.compute.amazonaws.com:8000/api-corner-ai/corner_ai/"
 
-        # files = {'picture': (self.picture.name, image_data)}
-        # data = {'picture_id': self.picture_id}
-        # response = requests.post(target_server_url, files=files, data=data)
+        files = {'picture': (self.picture.name, image_data)}
+        data = {'picture_id': self.picture_id}
+        response = requests.post(target_server_url, files=files, data=data)
 
-        # if response.status_code == 201:
-        #     print("image transfer success")
-        # else:
-        #     print("failed to send image")
+        if response.status_code == 201:
+            print("image transfer success")
+        else:
+            print("failed to send image")
 
-        # # ai 서버에서 정보 data GET으로 가져오기  (근데 생각해보니 위에 POST만으로도 data 받아올 수 있지 않나?)
-        # try:
-        #     response = requests.get(target_server_url)
-        #     data = response.json()
-        #     for elem in data:
-        #         if elem["picture_id"] == self.picture_id:
-        #             print(elem)
-        #             self.info = elem["product_name"]
-        # except requests.RequestException as e:
-        #     print("error: ", str(e))
-        self.product_name = ''
+        # ai 서버에서 정보 data GET으로 가져오기  (근데 생각해보니 위에 POST만으로도 data 받아올 수 있지 않나?)
+        try:
+            response = requests.get(target_server_url)
+            data = response.json()
+            for elem in data:
+                if elem["picture_id"] == self.picture_id:
+                    print(elem)
+                    self.product_name = elem["product_name"]
+        except requests.RequestException as e:
+            print("error: ", str(e))
+
+
+        # 위에서 찍은 사진이 어떤 제품 사진인지 self.product_name에 있으므로 추가 정보 가져오기
         price_model = PriceTable.objects.get(name=self.product_name)
 
-        self.info = "이 제품은 테스트 제품입니다. 맛이 좋고 가격은 " +  price_model.price + "입니다."
+        self.info = "이 제품은 " + self.product_name + "입니다. 가격은 " +  price_model.price + "입니다."
 
         super().save(*args, **kargs)
