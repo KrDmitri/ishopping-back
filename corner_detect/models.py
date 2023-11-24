6258f9ec111fba788c7bedb702a1e329cbc96f55
bombom cho <drjreric@gmail.com>
2023-11-24 16:55:18 +0900

    revise

--- corner_detect/models.py
+++ corner_detect/models.py
@@ -27,31 +27,22 @@ class CornerImage(models.Model):
         ##### ai 서버로 사진 전송 #####
         image_data = self.picture.read()
 
-        target_server_url = "http://ec2-43-201-249-72.ap-northeast-2.compute.amazonaws.com:8080/api-corner-ai/corner_ai/"
+        target_server_url = "http://ec2-43-201-249-72.ap-northeast-2.compute.amazonaws.com:8080/api-corner-ai/process-image/"
 
         files = {'picture': (self.picture.name, image_data)}
         data = {'picture_id': self.picture_id}
         response = requests.post(target_server_url, files=files, data=data)
 
-        if response.status_code == 201:
+        if response.status_code == 200 or response.status_code == 201:
             print("image transfer success")
         else:
             print("failed to send image")
+        
+        data = response.json()
+        self.product_name = data["product_name"]
+        print(data)
 
-        # ai 서버에서 정보 data GET으로 가져오기  (근데 생각해보니 위에 POST만으로도 data 받아올 수 있지 않나?)
-        try:
-            response = requests.get(target_server_url)
-            data = response.json()
-            for elem in data:
-                if elem["picture_id"] == self.picture_id:
-                    print(elem)
-                    self.product_name = elem["product_name"]
-        except requests.RequestException as e:
-            print("error: ", str(e))
-
-
-        # 위에서 찍은 사진이 어떤 제품 사진인지 self.product_name에 있으므로 추가 정보 가져오기
-        price = ""
+        # # 위에서 찍은 사진이 어떤 제품 사진인지 self.product_name에 있으므로 추가 정보 가져오기
         try:
             price_model = PriceTable.objects.get(name=self.product_name)
             price = price_model.price
