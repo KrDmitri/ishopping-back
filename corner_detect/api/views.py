621d44754edd24ff39c368c3d238cabb2669f45b
bombom cho <drjreric@gmail.com>
2023-12-14 15:12:59 +0900

    try2

--- corner_detect/api/views.py
+++ corner_detect/api/views.py
@@ -91,6 +91,7 @@ def get_location_view(request):
             lati = float(data.get('latitude'))
             longi = float(data.get('longitude'))
             nowPos = (lati, longi)
+            print(nowPos)
 
             stores = LocationTable.objects.all()
             storeList = []
@@ -101,9 +102,8 @@ def get_location_view(request):
                 tempDist = int(calculate_distance(nowPos, storePos))
                 storeList.append([store.name, tempDist, store.address])
             storeList.sort(key=lambda x:x[1])
-            print('good')
             serialized_list = json.dumps(storeList)
-            return JsonResponse({'store_list': storeList})
+            return JsonResponse({'store_list': serialized_list})
         except LocationTable.DoesNotExist:
             return JsonResponse({'error': 'DB not found'}, status=404)
         except Exception as e:
